a73886ab0add43515fafe83cced1f293d6ddcb3b
gbrozdev <gbrozdev@gmail.com>
2021-09-25 06:44:01 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,20 @@ var router = express.Router();
 var db = require('../connection')
 var ObjectId = require('mongodb').ObjectId
 /* GET home page. */
-router.get('/', async function(req, res) {
+router.get('/', function(req, res) {
     res.render('index');
+});
+router.get('/notes',  function(req, res) {
+    res.render('notes/notes');
+});
+router.get('/passwords',  function(req, res) {
+    res.render('passwords/passwords');
+});
+router.get('/find',  function(req, res) {
+    res.render('find/find');
+});
+router.get('/links',  function(req, res) {
+    res.render('links/links');
 });
 
 
