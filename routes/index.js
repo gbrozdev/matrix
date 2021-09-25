bd1e64d18001c3035d21d22d1f2f6d96a9d1c7fe
gbrozdev <gbrozdev@gmail.com>
2021-09-25 19:18:22 +0530

    login signup done

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 var db = require('../connection')
 var ObjectId = require('mongodb').ObjectId
+
 /* GET home page. */
 router.get('/', function(req, res) {
     res.render('index');
 });
-router.get('/notes',  function(req, res) {
-    res.render('notes/notes');
+router.get('/notes', async function(req, res) {
+    let notes = await db.get().collection('notes').find().toArray()  
+    res.render('notes/notes',{notes});
 });
+
 router.get('/passwords',  function(req, res) {
     res.render('passwords/passwords');
 });
@@ -18,20 +21,58 @@ router.get('/find',  function(req, res) {
 router.get('/links',  function(req, res) {
     res.render('links/links');
 });
+
+router.get('/products', async function(req, res) {
+    let products = await db.get().collection('products').find().toArray() 
+    res.render('products/products',{products});
+});
+router.get('/products/:id', async function(req, res) {
+    let id = req.params.id
+    let product = await db.get().collection('products').findOne({_id:ObjectId(id)})
+    res.render('products/productlist',{product});
+});
+
 router.get('/newnote',  function(req, res) {
     res.render('notes/newnote');
 });
 router.post('/newnote',  function(req, res) {
     console.log(req.body);
-    var note = req.body
+    let note = req.body
     note.time = new Date().toLocaleTimeString()
     note.date = new Date().toLocaleDateString()
     console.log(note);
-    db.get().collection('notes').insertOne(note).then((res)=>{
-        console.log(res.insertedId);
+    db.get().collection('notes').insertOne(note).then(async (response)=>{
+        let id = response.insertedId
+        let note = await db.get().collection('notes').findOne({_id:ObjectId(id)})        
+        res.render('notes/note',{note} );
     })
-    res.render('notes/notes');
+});
+router.get('/newproduct',  function(req, res) {
+    res.render('products/newproduct');
+});
+router.post('/newproduct',  function(req, res) {
+    let product = req.body;
+    db.get().collection('products').insertOne(product).then(async(response)=>{
+        let id = response.insertedId
+        let product = await db.get().collection('products').findOne({_id:ObjectId(id)})        
+        res.render('products/productlist',{product} );
+    })
+});
+router.get('/note/:id', async function(req, res) {
+    let id = req.params.id
+    let note = await db.get().collection('notes').findOne({_id:ObjectId(id)})   
+    res.render('notes/note',{note});
 });
 
-
+router.post('/search', async function(req, res) {
+    var content = req.body.search
+    let notes = await db.get().collection('notes').findOne({title:content})
+    console.log(notes);
+    if (notes) {
+        res.render('notes/notes',{notes,search:true})      
+    } else {
+        let notes = await db.get().collection('notes').find().toArray()  
+        res.render('notes/notes',{notes,err:true});
+        }
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../connection')
+var fun = require('../functions')
+
 
 /* GET users listing. */
 
 router.get('/', function(req, res) {
   res.render('admin');
 });
+router.get('/signup',(req,res)=>{
+  res.render('signup')
+})
+router.post('/signup',(req,res)=>{
+  fun.doSignup(req.body).then((user)=>{
+    console.log(user);
+    res.redirect('/users/login')
+  })
 
+})
+router.get('/login',  function(req, res) {
+  res.render('login');
+});
+
+router.post('/login',(req,res)=>{
+  fun.doLogin(req.body).then((user)=>{
+    console.log(user);
+    res.redirect('index')
+  })
+})
 module.exports = router;
